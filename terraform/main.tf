dbd55e3964ecb639c4a5cf41a384b706cf3b941d
nikita nand <105876777+Nikita130520@users.noreply.github.com>
2025-03-24 19:15:33 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,14 +4,13 @@ module "eks" {
   cluster_version = "1.27"
   vpc_id          = "vpc-02ed5a76fdcf53cf0"
 
-  vpc_config = {
-    subnet_ids = [
-      "subnet-0190bf6903dea0c87",
-      "subnet-02dc6d86a5bf19dbb"
-    ]
-  }
+  subnet_ids = [
+    "subnet-0190bf6903dea0c87",
+    "subnet-02dc6d86a5bf19dbb"
+  ]
 
-  node_groups = {
+
+    node_groups = {
     eks_nodes = {
       desired_capacity = 2
       max_capacity     = 3
