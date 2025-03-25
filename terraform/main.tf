501ca30cce92ce302050294aa6fcbbffda35e894
nikita nand <105876777+Nikita130520@users.noreply.github.com>
2025-03-25 16:13:55 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,30 @@
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "jenkins-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "jenkins-eks-cluster"
   cluster_version = "1.27"
   vpc_id          = "vpc-02ed5a76fdcf53cf0"
+  cluster_iam_role_name = aws_iam_role.eks_cluster_role.name
 
   subnet_ids = [
     "subnet-0190bf6903dea0c87",
@@ -12,12 +34,44 @@ module "eks" {
   # Use custom security group
   cluster_security_group_id = aws_security_group.eks_sg.id
 
+resource "aws_iam_role" "eks_node_role" {
+  name = "jenkins-eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_container_registry" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+  
   eks_managed_node_groups = {
     eks_nodes = {
       desired_size  = 2
       max_size      = 3
       min_size      = 1
       instance_types = ["t3.medium"]
+      iam_role_arn  = aws_iam_role.eks_node_role.arn
 
       # Attach security group to nodes
       node_security_group_tags = {
