5717435a1634d74e9e32748f810c95636f5e25b6
nikita nand <105876777+Nikita130520@users.noreply.github.com>
2025-04-01 18:05:30 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,3 +2,16 @@ provider "aws" {
   region = var.aws_region
 }
 
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
+data "aws_eks_cluster" "eks" {
+  name = "jenkins-eks-cluster"
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = data.aws_eks_cluster.eks.name
+}
