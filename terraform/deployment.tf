db9633c5aeff485049477d1631772af3e154bda7
nikita nand <105876777+Nikita130520@users.noreply.github.com>
2025-04-01 16:31:01 +0530

    Create deployment.tf

---
+++ terraform/deployment.tf
@@ -0,0 +1,47 @@
+resource "kubernetes_deployment" "app" {
+  metadata {
+    name      = "my-app"
+    namespace = "default"
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "my-app"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "my-app"
+        }
+      }
+      spec {
+        container {
+          name  = "my-app"
+          image = "dockerhub_username/my-app:v1"  # Change to your DockerHub image
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "app" {
+  metadata {
+    name = "my-app-service"
+  }
+  spec {
+    selector = {
+      app = "my-app"
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+    type = "LoadBalancer"
+  }
+}
