510ae7a30eeb76a7e28eed77d27a016bbd0edfe7
nikita nand <105876777+Nikita130520@users.noreply.github.com>
2025-04-01 16:40:04 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,3 +7,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
 }
+
+data "aws_eks_cluster_auth" "eks" {
+  name = aws_eks_cluster.eks.name
+}
