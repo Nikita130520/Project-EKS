67e6c1bc0de38ae98b2ac7b9911b35935bb9f9fd
nikita nand <105876777+Nikita130520@users.noreply.github.com>
2025-07-15 15:42:12 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,10 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "jenkins-eks-cluster"
   cluster_version = "1.27"
-  vpc_id          = "vpc-02ed5a76fdcf53cf0"
+  vpc_id          = "vpc-0658fa45bcc2e39ea"
   subnet_ids = [
-    "subnet-0190bf6903dea0c87",
-    "subnet-02dc6d86a5bf19dbb"
+    "subnet-012cb82e5506bf371",
+    "subnet-0cbb78f21d006b468"
   ]
 
 
